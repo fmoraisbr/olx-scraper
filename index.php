39b6f94e4ceedad9e4567af7344b2ee221aa0a5b
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-25 15:36:48 -0300

    Alteração

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
                         <table class="table branco">
   <thead>
     <tr>
-      <th scope="col">#</th>
+      <th scope="col">Nº</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
@@ -98,6 +98,38 @@
     </tr>
   </tbody>
 </table>
+
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
 
                     </div>
                 </div>
