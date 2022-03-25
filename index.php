8baab8130a0ef08bb87312f7b73cb82accc02594
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-25 14:50:23 -0300

    Alteração

--- index.php
+++ index.php
@@ -47,7 +47,21 @@
 
                             <?php
 
-echo "OI";
+                                $content = file_get_contents('https://df.olx.com.br/distrito-federal-e-regiao/brasilia/ra-xix---candangolandia/imoveis/aluguel?pe=1500&roe=3&ros=2&sf=1');
+
+                                preg_match_all('/<span color="dark" aria-label="(.*?)" class="wlwg1t-1 fsgKJO sc-ifAKCX eLPYJb" font-weight="400">(.*?)<\/span>/s', $content, $matchesx);
+                                preg_match_all('/<span color="dark" class="wlwg1t-1 fsgKJO sc-ifAKCX eLPYJb" font-weight="400">(.*?)<\/span>/s', $content, $matchesy);
+                                preg_match_all('/<div class="aoie8y-0 hRScWw">(.*?)<\/div>/s', $content, $matchesz);
+
+                                echo "<br>";
+
+                                $numeros = array("0","1","2","3","4");
+
+                                foreach ($numeros as $key => $value){
+                                    echo "<h2>".$matchesx[0][$value]." às ";
+                                    echo $matchesy[0][$value]."<br/>";
+                                    echo $matchesz[0][$value]."<br/></h2>";
+                                }
 
                             ?>
 
