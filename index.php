27ed56a71194865dbcede3af37d03f4b47856623
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-25 15:25:55 -0300

    Alteração

--- index.php
+++ index.php
@@ -67,6 +67,38 @@
 
                         </p> <!--
                         <a class="btn btn-primary btn-xl" href="#about">Find Out More</a> -->
+
+                        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
                     </div>
                 </div>
             </div>
