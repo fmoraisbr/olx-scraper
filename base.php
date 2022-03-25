5e38fc4a51769656dfcb63c57df4c58b2d203c46
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-25 14:00:23 -0300

    alterações

--- base.php
+++ base.php
@@ -6,6 +6,25 @@ preg_match_all('/<span color="dark" aria-label="(.*?)" class="wlwg1t-1 fsgKJO sc
 preg_match_all('/<span color="dark" class="wlwg1t-1 fsgKJO sc-ifAKCX eLPYJb" font-weight="400">(.*?)<\/span>/s', $content, $matchesy);
 preg_match_all('/<div class="aoie8y-0 hRScWw">(.*?)<\/div>/s', $content, $matchesz);
 
+echo "<br>";
+
+$numeros = array("0","1","2","3","4");
+
+foreach ($numeros as $key => $value){
+    echo "<h2>".$matchesx[0][$value]." às ";
+    echo $matchesy[0][$value]."<br/>";
+    echo $matchesz[0][$value]."<br/></h2>";
+}
+
+
+
+
+
+
+
+
+
+
 //preg_match_all('/<div class="wlwg1t-0 hWBHAm">(.*?)<\/div>/s', $content, $matches);
 //preg_match_all('/<div class="fnmrjs-7 erUydy">(.*?)<\/div>/s', $content, $matches); //wlwg1t-1 fsgKJO sc-ifAKCX eLPYJb
 
@@ -46,15 +65,5 @@ print_r($matchesx);
 echo "<br><br><hr><br>";
 
 var_dump($matchesx);*/
-
-echo "<br>";
-
-$numeros = array("0","1","2","3","4");
-
-foreach ($numeros as $key => $value){
-    echo "<h2>".$matchesx[0][$value]." às ";
-    echo $matchesy[0][$value]."<br/>";
-    echo $matchesz[0][$value]."<br/></h2>";
-}
 
 ?>
