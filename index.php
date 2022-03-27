2cb4d1982a2a32db91fe2196f180b86d5dc23281
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-27 20:54:44 -0300

    alteração

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 
                         $numeros = array("0", "1", "2", "3", "4");
 
-                        echo "<h1>".print_r($matchesk)."</h1>";
+                        echo "<h1>".$matchesk[0][1]."</h1>";
 
                         ?>
 
