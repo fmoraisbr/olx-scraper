c9b48a7c407da9d7dbb53940064ba30db4e9a264
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-28 17:09:27 -0300

    Alteração

--- zaptest.php
+++ zaptest.php
@@ -1,23 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function send_whatsapp($message = "Test")
-{
-    $phone = "+556186535340";  // Enter your phone number here
-    $apikey = "288162";       // Enter your personal apikey received in step 3 above
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes</title>
+</head>
 
-    $url = 'https://api.callmebot.com/whatsapp.php?source=php&phone=' . $phone . '&text=' . urlencode($message) . '&apikey=' . $apikey;
+<body>
 
-    if ($ch = curl_init($url)) {
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $html = curl_exec($ch);
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        // echo "Output:".$html;  // you can print the output for troubleshooting
-        curl_close($ch);
-        return (int) $status;
-    } else {
-        return false;
+    <form class="form-inline d-flex justify-content-center" style="border-radius: 4px;" action="tests.php" method="post">
+        <div class="input-group">
+            <input name="entrada" type="text" class="form-control" placeholder="Link do resultado" id="form1">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">Pesquisar</button>
+            </div>
+        </div>
+    </form>
+
+    <?php
+
+    function send_whatsapp($message = "Test")
+    {
+        $phone = "+556186535340";  // Enter your phone number here
+        $apikey = "288162";       // Enter your personal apikey received in step 3 above
+
+        $url = 'https://api.callmebot.com/whatsapp.php?source=php&phone=' . $phone . '&text=' . urlencode($message) . '&apikey=' . $apikey;
+
+        if ($ch = curl_init($url)) {
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            $html = curl_exec($ch);
+            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            // echo "Output:".$html;  // you can print the output for troubleshooting
+            curl_close($ch);
+            return (int) $status;
+        } else {
+            return false;
+        }
     }
-}
 
-send_whatsapp("this is a test from PHP");    //Text message that your would like to send
+    if (isset($_POST['entrada'])) {
+
+        $result = $_POST['entrada'];
+
+        send_whatsapp($result);
+
+        echo "<h1>" . $result . "</h1>";
+    }
+
+    ?>
+
+</body>
+
+</html>
