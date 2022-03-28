17b245a1764ced870ec1c7d376480aa3b094c609
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-28 16:26:31 -0300

    Alteração

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
 <body id="page-top">
     <!-- Navigation-->
 
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-light py-3" id="mainNav">
+        <!-- fixed-top -->
         <div class="container px-4 px-lg-5">
             <a class="navbar-brand" href="#page-top">OLX Scraper</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="" id="navbarResponsive">
-                <!-- collapse navbar-collapse -->
+            <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
                     <!-- <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
