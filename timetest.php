c181ad8e932018bd40d7c50523129e9a81629d79
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-28 18:49:08 -0300

    Alteração

--- timetest.php
+++ timetest.php
@@ -1,10 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "<script type='javascript'>alert('Email enviado com Sucesso!');</script>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes</title>
+</head>
 
-// while (1  === 1) {
+<body>
 
-//     echo "<script type='javascript'>alert('Email enviado com Sucesso!');</script>";
+    <form class="form-inline d-flex justify-content-center" style="border-radius: 4px;" action="tests.php" method="post">
+        <div class="input-group">
+            <input name="entrada" type="text" class="form-control" placeholder="Link do resultado" id="form1">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">Pesquisar</button>
+            </div>
+        </div>
+    </form>
 
-//     sleep(30);
-// }
+    <script type='javascript'>alert('Email enviado com Sucesso!');</script>
+
+    <?php
+
+    // echo "<script type='javascript'>alert('Email enviado com Sucesso!');</script>";
+
+    // while (1  === 1) {
+
+    //     echo "<script type='javascript'>alert('Email enviado com Sucesso!');</script>";
+
+    //     sleep(30);
+    // }
+
+    ?>
+
+</body>
+
+</html>
