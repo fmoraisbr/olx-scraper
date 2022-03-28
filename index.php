892084d75904c501fe0edf3cfaac9c5663827cff
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-28 18:40:11 -0300

    Alteração

--- index.php
+++ index.php
@@ -176,7 +176,7 @@
                                 /* [3] ------------------------ [START] Hora */
 
                                 $captura4 = $matchesy[0][$value];
-                                $link4 = substr($captura4, 79, 200);
+                                $link4 = substr($captura4, 78, 200);
                                 $aspaslink4 = str_replace('</span>', '', $link4); // echo "<td>" . $matchesy[0][$value] . "</td>";
 
                                 echo "<td>" . $aspaslink4 . "</td>";
