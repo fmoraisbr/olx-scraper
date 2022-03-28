c0d2b616dfc03518f8696111362cfbf1cbfec329
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-28 18:45:49 -0300

    Alteração

---
+++ timetest.php
@@ -0,0 +1,10 @@
+<?php
+
+while (1  === 1) { // 1 sempre será 1, sempre irá continuar!
+
+    // código
+
+    echo "<script type='javascript'>alert('Email enviado com Sucesso!');</script>";
+
+    sleep(30); // 30 segundos de pausa!
+}
