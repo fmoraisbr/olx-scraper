64817c2897f354bfd8c872eeb324c6785b5a0f39
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-29 18:30:21 -0300

    Alteração

--- index.php
+++ index.php
@@ -250,7 +250,7 @@
 
 
                                 if ($link3 === "Hoje") {
-                                    $mensagem = "Alerta de Aluguel: %0a" . $aspaslink;
+                                    $mensagem = "Alerta de Aluguel: %0a /n <br/> " . $aspaslink;
                                     send_whatsapp($mensagem);
                                 } else {
                                     echo "<h1> FUNCIONANDO UNOF! " . $link3 . " </h1><br/>";
