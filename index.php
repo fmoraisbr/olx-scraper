8fa2488dbef9f7ca7fa4c7117a70d3dbbd674f09
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-29 00:41:33 -0300

    alteração

--- index.php
+++ index.php
@@ -191,14 +191,18 @@
 
                                 if (($integer1 * (-1)) > 1300) {
                                     if (empty($integer1)) {
-                                        echo "<td style=\"color: white;\">NULL</td>";
+                                        if (isset($integer1)) {
+                                            echo "<td style=\"color: white;\">NULL</td>";
+                                        }
                                     } else {
                                         echo "<td style=\"color: red;\">R$ " . ($integer1 * (-1)) . "</td>";
                                     }
                                 }
                                 if (($integer1 * (-1)) < 1300) {
                                     if (empty($integer1)) {
-                                        echo "<td style=\"color: white;\">NULL</td>";
+                                        if (isset($integer1)) {
+                                            echo "<td style=\"color: white;\">NULL</td>";
+                                        }
                                     } else {
                                         echo "<td style=\"color: lawngreen;\">R$ " . ($integer1 * (-1)) . "</td>";
                                     }
