9b80cbfd9f594dfa28d8788952c9be598614ad2a
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-29 17:54:30 -0300

    Alteração

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
 
                     <p>
                         <?php
-                        echo "<h5 style=\"color: white; font-size: 11px; text-align: left;\">Total: " . $contador . " resultados</h5><br/>" . var_dump($matchesz);
+                        echo "<h5 style=\"color: white; font-size: 11px; text-align: left;\">Total: " . $contador . " resultados</h5><br/>" . $matchesz[0][10];
                         ?>
                     </p>
 
