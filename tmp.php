ce0416802fe00a418bfb7f472b8a043c8cee3624
Felipe Morais <contato.felipeamorim@gmail.com>
2022-03-30 23:35:04 -0300

    alteração

---
+++ tmp.php
@@ -0,0 +1,20 @@
+function send_whatsapp($message = "Test")
+{
+
+$phone = "+556186535340"; // Enter your phone number here
+$apikey = "288162"; // Enter your personal apikey received in step 3 above
+
+$url = 'https://api.callmebot.com/whatsapp.php?source=php&phone=' . $phone . '&text=' . urlencode($message) . '&apikey=' . $apikey;
+
+if ($ch = curl_init($url)) {
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$html = curl_exec($ch);
+$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+// echo "Output:".$html; // you can print the output for troubleshooting
+curl_close($ch);
+return (int) $status;
+} else {
+return false;
+}
+}
