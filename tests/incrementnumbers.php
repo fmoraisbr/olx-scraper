589723f5180857eec6c4094456de31ab13c87b71
Felipe Morais <contato.felipeamorim@gmail.com>
2022-05-06 15:17:09 -0300

    Update incrementnumbers.php

--- tests/incrementnumbers.php
+++ tests/incrementnumbers.php
@@ -1,7 +1,11 @@
 <?php
 
-$arr = array(1, 2, 3, 4);
+$arr = array(1, 2, 3, 4, 2, 2);
 
 foreach ($arr as $chave => $valor) {
-    echo $valor;
+    if ($arr[$valor] === 2) {
+        $somador = $arr[$valor];
+    }
 }
+
+var_dump($somador);
